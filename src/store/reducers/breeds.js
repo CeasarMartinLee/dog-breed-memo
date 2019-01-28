32a2ca5ed5a2dd6fa766bb6b913a165030cbed94
ssikoron <ssikoron@gmail.com>
2019-01-28 17:29:46 +0100

    Code cleanup + .editorconfig

--- src/store/reducers/breeds.js
+++ src/store/reducers/breeds.js
@@ -1,9 +1,9 @@
 export default (state = [], action = {}) => {
-  return [
-    {
+  return {
+    'breedName': {
       breedName: 'alpaca',
       images: ['url1', 'url2'],
       hasAlreadyAppeared: false
     }
-  ]
+  }
 }
